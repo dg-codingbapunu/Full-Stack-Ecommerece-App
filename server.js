ef0f2003fedb8af4c935778328d79aa4ddaaa9d3
patitapaban <patitapabanpanda046@gmail.com>
2024-11-28 14:07:30 +0530

    commit

--- server.js
+++ server.js
@@ -7,48 +7,48 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url"; // Import required for ES module paths
 
 dotenv.config();
 
-// database congig
-
+// database config
 connectDB();
 
 // rest object---
-
 const app = express();
 
-// midlewares
-
+// middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
     origin: "http://localhost:5173", // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    // Include credentials (if needed)
-    credentials: true,
+    credentials: true, // Include credentials (if needed)
   })
 );
 
-//  routes
-
+// routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//  rest --- api
-
+// rest --- api
 app.get("/", (req, res) => {
   res.send("hello welcome to my app");
 });
 
 // Static files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); // Derive the directory name
 
 app.use(express.static(path.join(__dirname, "./client/ecom/dist")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/ecom/dist/index.html"));
 });
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
