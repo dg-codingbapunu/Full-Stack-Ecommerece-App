29cecbc06d23d44884859c48f48d9617d0f119cd
patitapaban <patitapabanpanda046@gmail.com>
2024-11-28 21:22:54 +0530

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://patitapaban-ecommerce-app.onrender.com", // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     // Include credentials (if needed)
     credentials: true,
