b095771aecb4867e52d99e45256b7da7153ec4d8
patitapaban <patitapabanpanda046@gmail.com>
2024-11-29 20:19:39 +0530

    edit

--- client/ecom/vite.config.js
+++ client/ecom/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // Backend server
+        target: "https://full-stack-ecommerece-backend.vercel.app/", // Backend server
         changeOrigin: true, // Changes the origin of the host header to the target URL
         secure: false, // If using HTTPS without a valid certificate
       },
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://mern-frontend-liart.vercel.app/", // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     // Include credentials (if needed)
     credentials: true,
