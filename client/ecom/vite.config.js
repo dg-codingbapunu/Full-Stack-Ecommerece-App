7e4a72d7c2821ce9db0bd5d58143e661e9855afb
patitapaban <patitapabanpanda046@gmail.com>
2024-12-03 00:19:11 +0530

    update build

--- client/ecom/vite.config.js
+++ client/ecom/vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // Ensures the output directory is correctly set
+  },
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "https://patitapaban-ecommerce.onrender.com", // Backend server
         changeOrigin: true, // Changes the origin of the host header to the target URL
-        secure: true, // If using HTTPS without a valid certificate
+        secure: true, // If the backend uses HTTPS with an invalid certificate
       },
     },
   },
