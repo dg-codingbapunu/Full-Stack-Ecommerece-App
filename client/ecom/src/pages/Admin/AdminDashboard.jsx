4daa33d2a178afa506e7779a9cb09f6dcac71c11
patitapaban <patitapabanpanda046@gmail.com>
2025-04-14 17:19:36 +0530

    update Adminpage

--- client/ecom/src/pages/Admin/AdminDashboard.jsx
+++ client/ecom/src/pages/Admin/AdminDashboard.jsx
@@ -16,7 +16,7 @@ const AdminDashboard = () => {
             <div className="card w-75 p-3">
               <h3>Admin Name: {auth?.user?.name}</h3>
               <h3>Admin Email: {auth?.user?.email}</h3>
-              <h3>Admin Contact: {auth?.user?.phone}</h3>
+              <h3>Admin Contacts: {auth?.user?.phone}</h3>
             </div>
           </div>
         </div>
